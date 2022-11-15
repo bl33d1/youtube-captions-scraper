d49110d9fa61d14eec671666e9fb3b502e184746
bl33d1 <bledibllaca12@gmail.com>
2022-11-15 03:04:12 +0100

    init commit // working ver

--- src/index.js
+++ src/index.js
@@ -1,69 +1,22 @@
-/* @flow */
+let getSubtitles = require('youtube-captions-scraper').getSubtitles;
+const fs = require('fs');
 
-import he from 'he';
-import axios from 'axios';
-import { find } from 'lodash';
-import striptags from 'striptags';
-
-export async function getSubtitles({
-  videoID,
-  lang = 'en',
-}: {
-  videoID: string,
-  lang: 'en' | 'de' | 'fr' | void,
-}) {
-  const { data } = await axios.get(
-    `https://youtube.com/watch?v=${videoID}`
-  );
-
-  // * ensure we have access to captions data
-  if (!data.includes('captionTracks'))
-    throw new Error(`Could not find captions for video: ${videoID}`);
-
-  const regex = /({"captionTracks":.*isTranslatable":(true|false)}])/;
-  const [match] = regex.exec(data);
-  const { captionTracks } = JSON.parse(`${match}}`);
-
-  const subtitle =
-    find(captionTracks, {
-      vssId: `.${lang}`,
-    }) ||
-    find(captionTracks, {
-      vssId: `a.${lang}`,
-    }) ||
-    find(captionTracks, ({ vssId }) => vssId && vssId.match(`.${lang}`));
-
-  // * ensure we have found the correct subtitle lang
-  if (!subtitle || (subtitle && !subtitle.baseUrl))
-    throw new Error(`Could not find ${lang} captions for ${videoID}`);
-
-  const { data: transcript } = await axios.get(subtitle.baseUrl);
-  const lines = transcript
-    .replace('<?xml version="1.0" encoding="utf-8" ?><transcript>', '')
-    .replace('</transcript>', '')
-    .split('</text>')
-    .filter(line => line && line.trim())
-    .map(line => {
-      const startRegex = /start="([\d.]+)"/;
-      const durRegex = /dur="([\d.]+)"/;
-
-      const [, start] = startRegex.exec(line);
-      const [, dur] = durRegex.exec(line);
-
-      const htmlText = line
-        .replace(/<text.+>/, '')
-        .replace(/&amp;/gi, '&')
-        .replace(/<\/?[^>]+(>|$)/g, '');
-
-      const decodedText = he.decode(htmlText);
-      const text = striptags(decodedText);
-
-      return {
-        start,
-        dur,
-        text,
-      };
-    });
-
-  return lines;
-}
+getSubtitles({
+  videoID: 'ATlila3e9dM', // youtube video id
+}).then(function(captions) {
+  // console.log(captions);
+  let captionString;
+  captions.map((text) => {
+    let captionSentence = text.text;
+    captionString+=text.text;
+    if(captionSentence.charAt(captionSentence.length-1) === '.' ||
+      captionSentence.charAt(captionSentence.length-1) != ' '
+    ){
+      captionString+="\n";
+    }
+  })
+  //console.log(captionString);
+  //const captionsString = JSON.stringify(captions[0].text.toString(), null, 4);
+  fs.writeFileSync('./first-punic-war-text-only.json', captionString);
+  //console.log(captions)
+});
